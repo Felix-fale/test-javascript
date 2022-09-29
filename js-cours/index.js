11eb8e7676f7128643ba0276d1df6c887e449c86
Felix-fale <falefelix23@gmail.com>
2022-09-29 15:23:36 +0000

    les variable et les fonctions

---
+++ js-cours/index.js
@@ -0,0 +1,10 @@
+// functions
+function faireQuelqueChose() {
+    console.log('je fais un truc');
+}
+const faireUneTache = (top) => {
+    console.log('je fais :' + top);
+}
+
+faireUneTache('une tache');
+faireUneTache('une course');
